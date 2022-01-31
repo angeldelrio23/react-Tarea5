25f42dd1d6fde95089a62ec880df4dcc793e98ad
angeldelrio23 <angeldelrio23@yahoo.es>
2022-01-31 14:01:49 +0100

    1 file modified, 4 added

---
+++ src/data/DatosJuegos.js
@@ -0,0 +1,46 @@
+export const TitulosTablaJuegos = {
+  id: 'Posición',
+  field1: 'Nombre',
+  field2: 'Desarroladora',
+  field3: 'PEGI',
+};
+
+export const DatosTablaJuegos = [
+  {
+    posicion: '1',
+    nombre: 'League of Legends',
+    desarroladora: 'Riot Games',
+    pegi: '+3',
+    imagen:
+      'https://www.esportmaniacos.com/wp-content/uploads/2021/08/LoL2-1.jpg',
+    descripción: 'Un juego HORROROSO',
+  },
+  {
+    posicion: '2',
+    nombre: 'Elder Ring',
+    desarroladora: 'FromSoftware',
+    pegi: '+16',
+    imagen:
+      'https://media.tycsports.com/files/2021/08/27/324652/elden-ring-gameplay_1440x810_wmk.jpg',
+    descripción: 'Juego de rol y acción de ambientación fantástica',
+  },
+  {
+    posicion: '3',
+    nombre: 'Lost Ark',
+    desarroladora: 'Smilegate',
+    pegi: '+12',
+    imagen:
+      'https://images.ctfassets.net/umhrp0op95v1/1FJm4Dq3u6lPIycfLmTnup/480baf949497bfc4bf33a13a7b9d60be/LostArk_HP_BG_SM.jpg',
+    descripción:
+      'Juego de rol de acción en línea multijugador masivo de fantasía isométrica 2.5D',
+  },
+  {
+    posicion: '4',
+    nombre: 'New World',
+    desarroladora: 'Amazon Studios',
+    pegi: '+12',
+    imagen:
+      'https://images.ctfassets.net/j95d1p8hsuun/29peK2k7Ic6FsPAVjHWs8W/06d3add40b23b20bbff215f6979267e8/NW_OPENGRAPH_1200x630.jpg',
+    descripción: 'Juego de rol multijugador masivo en línea',
+  },
+];
